d6e0db9a87b83bc28cb7721d8ef03d7887bec433
Mikepan1992 <981364490@qq.com>
2018-07-12 00:59:45 +0800

    7.12

---
+++ obj.js
@@ -0,0 +1,32 @@
+
+
+
+       var activity=[
+        {
+            "tiltle" : "限时特卖",
+            "subtitle":"正品低价 必买清单",
+            "imglink":"http://www.sjgo365.com/Content/newhome/images/normalAct01.png"
+        },
+        
+        {
+            "tiltle":"新品首发",
+            "subtitle":"botanist植物学家白盖无硅油洗发水清爽型",
+            "imglink":"http://www.sjgo365.com/Content/newhome/images/normalAct03.png"
+        },
+        {
+            "tiltle":"镇店之宝",
+            "subtitle":" 薄如蝉翼的'膜'力",
+            "imglink":"http://www.sjgo365.com/Content/newhome/images/24554212.png"
+        },
+        {
+            "tiltle":"新会员首单福利",
+            "subtitle":"红包秒杀享不停",
+            "imglink":"http://www.sjgo365.com/Content/newhome/images/normalAct06.png"
+        }
+        
+        ]
+       
+       
+    
+
+
